9a31eb8329700fa17033f21dc43dc3e5f6861bd1
Nyárai Gergő <nyariag@gmail.com>
2020-04-09 17:44:22 +0200

    --progress: REDIRECT: Redirect after login and register.

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -6,7 +6,7 @@ require_once(__DIR__.'/../service/AuthenticationService.php');
 class LoginController implements ControllerInterface{
     public function run(){
         if (AuthenticationService::authentication($_POST['email'], $_POST['password'])) {
-            return 'Bejelentkeztél!';
+            header('location:index.php');
         }
         else {
             return 'Helytelen email cím és jelszó páros!';
--- controller/RegisterController.php
+++ controller/RegisterController.php
@@ -6,7 +6,7 @@ class RegisterController implements ControllerInterface{
     public function run(){
         if (!UserService::getUserByEmail($_POST['email'])){
             UserService::createUser($_POST['email'], $_POST['password']);
-            return 'Sikeresen beregisztráltál!';
+            header('location:index.php');
         }
         else{
             return 'Van ilyen felhasználó';
